7c8acae978e390c27d64de119190456cfbd2a99e
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-01-24 16:43:31 +0300

    feat: implement read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import fs from 'fs/promises';
+import path from 'path';
 const read = async () => {
-    // Write your code here 
+    try {
+        const content = await fs.readFile(path.join(path.resolve(), 'src', 'fs', 'files', 'fileToRead.txt'), { encoding: 'utf8' });
+        console.log(content)
+    }
+    catch(err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
