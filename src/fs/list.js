6e61e160eaee3fc900cf48fc44cccdad9023e18b
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-01-25 01:05:55 +0300

    refactor: leave only file names in array

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,11 +1,15 @@
 import fs from 'fs/promises';
 import path from 'path';
 const list = async () => {
+    const arrayOfNames = [];
     try {
         const files = await fs.readdir(path.join(path.resolve(), 'src', 'fs', 'files'), { withFileTypes: true });
         if (files.length == 0) { throw new Error('FS operation failed') }
         else {
-            console.log(files)
+            for (let file of files) {
+              arrayOfNames.push(file['name']);
+            }
+            console.log(arrayOfNames);
         }
     }
     catch (err) {
