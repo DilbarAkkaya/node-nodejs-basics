41ef504ed2cdd8b2809dc087cad2303af5716d96
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-01-24 12:22:17 +0300

    refactor: change relative  path with absolute with using path api

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,7 +1,8 @@
 import fs from 'fs/promises';
+import path from 'path';
 const list = async () => {
     try {
-        const files = await fs.readdir('./src/fs/files', { withFileTypes: true });
+        const files = await fs.readdir(path.join(path.resolve(), 'src', 'fs', 'files'), { withFileTypes: true });
         if (files.length == 0) { throw new Error('FS operation failed') }
         else {
             console.log(files)
