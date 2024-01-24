e21dba47da3c789fb2c9eab7885d08f0f14f59f4
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-01-24 11:41:40 +0300

    feat: implement task list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
 const list = async () => {
-    // Write your code here 
+    try {
+        const files = await fs.readdir('./src/fs/files', { withFileTypes: true });
+        if (files.length == 0) { throw new Error('FS operation failed') }
+        else {
+            console.log(files)
+        }
+    }
+    catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
