d1bdd8f13363b262dcd50d59c7f84bc9d3c8f0ee
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-01-24 19:49:57 +0300

    feat: implement task env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const variablesWithRSS_ = [];
+    for (let key in process.env) {
+        if (key.slice(0, 4) === 'RSS_') {
+            variablesWithRSS_.push(`${key} = ${process.env[key]}`)
+        }
+    }
+    console.log(variablesWithRSS_.join('; '))
 };
 
 parseEnv();
