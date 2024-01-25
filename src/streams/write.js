4294330e4c791020cc9e495c0a7297ebf69ce845
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-01-25 22:41:53 +0300

    refactor: add flags for  keeping prew

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 const write = async () => {
-    const stream = fs.createWriteStream(path.join(path.resolve(), 'src', 'streams', 'files', 'fileToWrite.txt'));
+    const stream = fs.createWriteStream(path.join(path.resolve(), 'src', 'streams', 'files', 'fileToWrite.txt'), {flags: 'a'});
     process.stdin.pipe(stream);
 };
 
