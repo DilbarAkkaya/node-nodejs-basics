9775f13969a0a20f5196d67789857b71b3747c77
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-01-25 20:28:31 +0300

    feat: implement calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(path.join(path.resolve(), 'src', 'hash', 'files', 'fileToCalculateHashFor.txt'))
+    return new Promise((resolve, reject) => {
+        stream.on('data', (data) => {
+            hash.update(data);
+        });
+        stream.on('end', () => {
+            const valueHexFormat = hash.digest('hex');
+            resolve(valueHexFormat);
+            console.log(valueHexFormat);
+        });
+        stream.on('error', (error) => {
+            reject(error)
+        })
+    })
 };
 
 await calculateHash();
