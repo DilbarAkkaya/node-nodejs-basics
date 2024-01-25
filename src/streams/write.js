d7ce0d54accb068c686fd662073da2c57861674c
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-01-25 22:33:07 +0300

    feat: implement task write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,8 @@
+import fs from "fs";
+import path from "path";
 const write = async () => {
-    // Write your code here 
+    const stream = fs.createWriteStream(path.join(path.resolve(), 'src', 'streams', 'files', 'fileToWrite.txt'));
+    process.stdin.pipe(stream);
 };
 
 await write();
