04e0679db1dfd429d821836b83fdfc94b9954621
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-01-27 00:45:20 +0300

    feat: implement wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import path from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+    const arrayOfResultsFromWorkers = [];
+    const numberCPU = os.cpus().length;
+    for (let i = 0; i < numberCPU; i++) {
+        const worker = new Worker(path.join(path.resolve(), 'src', 'wt', 'worker.js'), { workerData: 10 + i });
+        worker.on('message', (message) => {
+            arrayOfResultsFromWorkers[i] = { status: 'resolved', data: message };
+            if (arrayOfResultsFromWorkers.length === numberCPU) console.log(arrayOfResultsFromWorkers);
+        });
+        worker.on('error', (error) => {
+            arrayOfResultsFromWorkers[i] = { status: 'error', data: null };
+            if (arrayOfResultsFromWorkers.length === numberCPU) {
+                console.log(arrayOfResultsFromWorkers);
+            }
+        });
+    }
+
 };
+
 
 await performCalculations();
